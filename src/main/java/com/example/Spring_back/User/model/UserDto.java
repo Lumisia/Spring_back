27f98828c1aaa2963bbe6acf5e0ddfbfbc137fe1
Lumisia <zh2171@gmail.com>
2026-02-24 11:46:39 +0900

    [Feat] User_Dto

---
+++ src/main/java/com/example/Spring_back/User/model/UserDto.java
@@ -0,0 +1,46 @@
+package com.example.Spring_back.User.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.extern.java.Log;
+
+public class UserDto {
+
+    @Getter
+    public static class SignupReq {
+        private String email;
+        private String user_id;
+        private String password;
+
+        public User toEntity() {
+            return User.builder()
+                    .email(this.email)
+                    .user_id(this.user_id)
+                    .password(this.password)
+                    .role("USER")
+                    .build();
+        }
+    }
+
+    @Getter
+    public static class LoginReq {
+        private String email;
+        private String password;
+    }
+
+    @Builder
+    @Getter
+    public static class LoginRes {
+        private Long idx;
+        private String email;
+        private String user_id;
+
+        public static LoginRes form(User entity) {
+            return LoginRes.builder()
+                    .idx(entity.getIdx())
+                    .email(entity.getEmail())
+                    .user_id(entity.getUser_id())
+                    .build();
+        }
+    }
+}
